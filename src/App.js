c2821f2a8db3e557070321b0bb93690912473fd8
Victor Araujo <victor.carvalho.araujo04@gmail.com>
2023-04-20 00:49:58 -0300

    - add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="http://localhost:3000/assets/img/logo/large_logo.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Building a React App with Webpack 5 and Babel 7
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
