aeaced3074fd85b87030a6ae97b9a0b6083371a6
Victor Araujo <victor.carvalho.araujo04@gmail.com>
2023-04-27 02:07:01 -0300

    - Add loader; - Add get of meta tags using api but trying with error in CORS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
+import { useState, useEffect } from 'react';
 import { NavBar } from './components/NavBar/index.js';
 import { Carrousel } from './components/Carrousel/index.js';
+import { Loader } from './components/Loader/index.js';
+
+const API_URL = "http://localhost/equal-learn/public/Api/";
+
+function getData() {
+  return fetch(API_URL + "MetaTags/getMetaTags").then((response) => response.json());
+}
 
 export const App = () => {
 
-  const route = 'http://localhost:3000/';
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getData().then((responseData) => setData(responseData)).catch((error) => console.error(error));
+  }, []);
+
+  if (!data) {
+    setInterval(() => {
+      //-- Call the API each 15 seconds
+      getData().then((responseData) => setData(responseData)).catch((error) => console.error(error));
+    }, 15000);
+
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
 
   return (
     <div className="row">
       <div className="col-12">
-
-        <NavBar url={route} profileUrl="assets/img/avatar.jpg" profileName="Cléber da Costa" />
-
+        <NavBar url={data.url} profileUrl="assets/img/avatar.jpg" profileName="Cléber da Costa" />
       </div>
       <div className="col-12">
         <main>
           <div className="container">
             <div className="card">
               <div className="card-body">
-
-                <Carrousel url={route} />
-
+                <Carrousel url={data.url} />
                 <h5 className="card-title">Título do card</h5>
-
                 <p className="card-text">Texto do card</p>
                 <a href="#" className="btn btn-primary">Ir para algum lugar</a>
               </div>
@@ -31,4 +51,4 @@ export const App = () => {
       </div>
     </div>
   );
-}
+};
---
+++ src/components/Loader/index.js
@@ -0,0 +1,28 @@
+import './style.css'
+
+export const Loader = () => {
+
+    return (
+        <div className="loading-container">
+            <div className="loader">
+                <div className="loader-inner">
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
+                    </div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
+                    </div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
+                    </div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
+                    </div>
+                    <div className="loader-line-wrap">
+                        <div className="loader-line"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
